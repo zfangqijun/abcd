5429af099f0b057a0ea79278c9d1345cd021ef28
FangQiJun <zfangqj@gmail.com>
2023-04-13 21:53:40 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,9 @@
-basic.forever(function () {
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("- - - - - - - - ", 120)
+})
+input.onGesture(Gesture.Shake, function () {
+	
 })
